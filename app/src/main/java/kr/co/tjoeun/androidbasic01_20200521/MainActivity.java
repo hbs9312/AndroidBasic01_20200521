5bc4049402aaee9e89c50ac84e3381d58ce3d146
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-21 11:43:33 +0900

    액티비티 호출 + 기존 액티비티 종료  => 조합하면 액티비티 이동.

--- app/src/main/java/kr/co/tjoeun/androidbasic01_20200521/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/androidbasic01_20200521/MainActivity.java
@@ -3,6 +3,7 @@ package kr.co.tjoeun.androidbasic01_20200521;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -27,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+//                FirstActivity로 이동.
+//                비행기 티켓 발권
+                Intent myIntent = new Intent(MainActivity.this, FirstActivity.class);
+//                실제로 출발
+                startActivity(myIntent);
+
+//                이 액티비티를 종료하는 코드 (이동처럼 보이도록 처리)
+//                finish();
             }
         });
     }
