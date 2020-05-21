9453674c0efedb8f44a214f1fdc880182031c554
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-21 11:55:19 +0900

    이전화면으로 돌아가기

--- app/src/main/java/kr/co/tjoeun/androidbasic01_20200521/FirstActivity.java
+++ app/src/main/java/kr/co/tjoeun/androidbasic01_20200521/FirstActivity.java
@@ -1,14 +1,45 @@
 package kr.co.tjoeun.androidbasic01_20200521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import kr.co.tjoeun.androidbasic01_20200521.databinding.ActivityFirstBinding;
 
 public class FirstActivity extends AppCompatActivity {
+
+    ActivityFirstBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_first);
+        setupEvents();
+        setValues();
+
+    }
+
+    void setupEvents() {
+
+        binding.goBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+
+//                이전화면으로 돌아갈때는 Intent를 사용하지 않는다.
+//                Intent는 새 화면을 만들어내는 기능이 포함되어 있다.
+//                  => 돌아가는게 아니라, 다른 새 화면을 얹어주는 효과가 발생.
+//                Intent myIntent = new Intent(FirstActivity.this, MainActivity.class);
+//                startActivity(myIntent);
+            }
+        });
+
+    }
+
+    void setValues() {
+
     }
 }
